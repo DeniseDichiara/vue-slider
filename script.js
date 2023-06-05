2517d0539bf64a5185834b113d81f7b4455336bc
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-05 19:44:12 +0200

    modified js

--- script.js
+++ script.js
@@ -4,13 +4,10 @@ createApp({
     data(){
         return {
             images : [
-                {link : 'https://holrmagazine.com/wp-content/uploads/2020/01/Next-in-Fashion-759x500.jpg'},
-                {link : 'https://cdn.shopify.com/s/files/1/0767/7835/files/next_in_fashion.jpg?v=1595019494'},
-                {link : 'https://cloudfront-eu-central-1.images.arcpublishing.com/thenational/6HIUGKNIBRCJ3KYK6AEIKM65TU.jpg'}
-
+                { link : 'https://holrmagazine.com/wp-content/uploads/2020/01/Next-in-Fashion-759x500.jpg' },
+                { link : 'https://cdn.shopify.com/s/files/1/0767/7835/files/next_in_fashion.jpg?v=1595019494' },
+                { link : 'https://cloudfront-eu-central-1.images.arcpublishing.com/thenational/6HIUGKNIBRCJ3KYK6AEIKM65TU.jpg' }
             ]
-            activeIndex : 0,
         }
-        
     },
 })
