a533a2bf85bcf0c528e76517e2e1e3f418aa6847
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-05 18:42:19 +0200

    modified obj array

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const { createApp } = Vue;
 createApp({
     data(){
         return {
-            images : {
-                link : 'https://holrmagazine.com/wp-content/uploads/2020/01/Next-in-Fashion-759x500.jpg',
-                link : 'https://flxt.tmsimg.com/assets/p23933116_b_h8_aa.jpg'
-            },
+            images : [
+                {link : 'https://holrmagazine.com/wp-content/uploads/2020/01/Next-in-Fashion-759x500.jpg'},
+                {link : 'https://cdn.shopify.com/s/files/1/0767/7835/files/next_in_fashion.jpg?v=1595019494'},
+                {link : 'https://cloudfront-eu-central-1.images.arcpublishing.com/thenational/6HIUGKNIBRCJ3KYK6AEIKM65TU.jpg'}
+
+            ]
             activeIndex : 0,
         }
         
