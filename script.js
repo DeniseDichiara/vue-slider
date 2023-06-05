e01d46b0a60ca4e9030a62b230e60e656c80090d
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-05 22:41:18 +0200

    added methods

--- script.js
+++ script.js
@@ -7,7 +7,16 @@ createApp({
                 { link : 'https://holrmagazine.com/wp-content/uploads/2020/01/Next-in-Fashion-759x500.jpg' },
                 { link : 'https://cdn.shopify.com/s/files/1/0767/7835/files/next_in_fashion.jpg?v=1595019494' },
                 { link : 'https://cloudfront-eu-central-1.images.arcpublishing.com/thenational/6HIUGKNIBRCJ3KYK6AEIKM65TU.jpg' }
-            ]
+            ],
+            activeIndex : 0,
         }
     },
+    methods: {
+        prev(){
+            this.activeIndex--;
+        },
+        nextImage() {
+            this.activeIndex++;
+        }
+    }
 })
