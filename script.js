fb9424ac6e4125520d5550a4437ca8475a26f14c
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-06 08:20:12 +0200

    modfied array images

--- script.js
+++ script.js
@@ -4,9 +4,27 @@ createApp({
     data(){
         return {
             images : [
-                { link : 'https://holrmagazine.com/wp-content/uploads/2020/01/Next-in-Fashion-759x500.jpg' },
-                { link : 'https://cdn.shopify.com/s/files/1/0767/7835/files/next_in_fashion.jpg?v=1595019494' },
-                { link : 'https://cloudfront-eu-central-1.images.arcpublishing.com/thenational/6HIUGKNIBRCJ3KYK6AEIKM65TU.jpg' }
+                {
+                    image: 'img/01.webp',
+                    title: 'Marvel\'s Spiderman Miles Morale',
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                }, {
+                    image: 'img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                }, {
+                    image: 'img/03.webp',
+                    title: 'Fortnite',
+                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                }, {
+                    image: 'img/04.webp',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                }, {
+                    image: 'img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                }
             ],
             activeIndex : 0,
         }
